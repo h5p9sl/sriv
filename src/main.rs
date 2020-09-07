a7e87f50511fb7f675228dc49a6bb2e37dea41bb
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-09-07 08:26:09 -0600

    Add basic texture creation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use clap::Arg;
-use clap::{crate_authors, crate_description, crate_name, crate_version};
-use glium::{program, uniform};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg};
+use glium::{program, texture, uniform};
 use image::DynamicImage;
 use std::path::Path;
 
-fn _load_image_from_path<P>(fp: P) -> Option<DynamicImage>
+// TODO: Add a verbosity variable
+macro_rules! log_verbose {
+    () => (std::eprint!("\n"));
+    ($($args:tt)*) => ({
+        eprintln!("[INFO]: {}", format_args!($($args)*));
+    })
+}
+
+fn load_image_from_path<P>(fp: P) -> Option<DynamicImage>
 where
     P: AsRef<Path>,
 {
+    log_verbose!("Loading image file...");
     let fp = fp.as_ref();
     let di = image::open(&fp);
     if let Some(e) = di.as_ref().err() {
@@ -20,60 +28,93 @@ where
     di.ok()
 }
 
-fn _construct_texture_from_imagefile<P>(_fp: P) -> Option<glium::Texture2d>
+fn construct_texture_from_imagefile<F, P>(display: &F, fp: P) -> Option<texture::SrgbTexture2d>
 where
+    F: glium::backend::Facade,
     P: AsRef<Path>,
 {
-    todo!();
+    if let Some(image) = load_image_from_path(fp) {
+        use image::GenericImageView;
+        log_verbose!("Creating texture...");
+
+        let raw_image_data: Vec<u8> = image.to_rgba().to_vec();
+        let raw_image_data = texture::RawImage2d::from_raw_rgba(raw_image_data, image.dimensions());
+        let texture = texture::SrgbTexture2d::new(display, raw_image_data);
+        return texture.ok();
+    }
+    None
 }
 
 fn main() {
     use glutin::{dpi, event_loop, window, Api, ContextBuilder, GlRequest};
 
-    println!("WARNING: This application is currently considered in early-alpha and non-functional");
+    eprintln!(
+        "WARNING: This application is currently considered in early-alpha and non-functional"
+    );
 
-    let _matches = clap::app_from_crate!()
+    let start_instant = std::time::Instant::now();
+
+    let matches = clap::app_from_crate!()
         .arg(
             Arg::with_name("file")
                 .help("Defines the file to use")
-                .required(false)
+                .required(true)
                 .multiple(false)
                 .index(1),
         )
         .get_matches();
+    let file_path = Path::new(matches.value_of("file").unwrap());
 
+    log_verbose!(
+        "Creating EventLoop: {}",
+        start_instant.elapsed().as_secs_f32()
+    );
     let el = event_loop::EventLoop::new();
+    log_verbose!("Building Window: {}", start_instant.elapsed().as_secs_f32());
     let wb = window::WindowBuilder::new()
         .with_title(crate_name!())
         .with_inner_size(dpi::LogicalSize::new(800.0, 600.0));
+    log_verbose!(
+        "Building Context: {}",
+        start_instant.elapsed().as_secs_f32()
+    );
     let windowed_context = ContextBuilder::new()
         .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
         .with_gl_profile(glutin::GlProfile::Compatibility)
         .build_windowed(wb, &el)
         .unwrap();
+    log_verbose!(
+        "Initializing Display: {}",
+        start_instant.elapsed().as_secs_f32()
+    );
     let display = glium::Display::from_gl_window(windowed_context).unwrap();
 
     let vbo = {
         #[derive(Copy, Clone)]
         struct Vertex {
             position: [f32; 2],
+            texcoord: [i32; 2],
         }
-        glium::implement_vertex!(Vertex, position);
+        glium::implement_vertex!(Vertex, position, texcoord);
 
         glium::VertexBuffer::new(
             &display,
             &[
                 Vertex {
-                    position: [-0.5, -0.5],
+                    position: [-1.0, -1.0],
+                    texcoord: [0, 1],
                 },
                 Vertex {
-                    position: [-0.5, 0.5],
+                    position: [-1.0, 1.0],
+                    texcoord: [0, 0],
                 },
                 Vertex {
-                    position: [0.5, 0.5],
+                    position: [1.0, 1.0],
+                    texcoord: [1, 0],
                 },
                 Vertex {
-                    position: [0.5, -0.5],
+                    position: [1.0, -1.0],
+                    texcoord: [1, 1],
                 },
             ],
         )
@@ -87,45 +128,70 @@ fn main() {
     )
     .unwrap();
 
+    log_verbose!(
+        "Compiling shaders...: {}",
+        start_instant.elapsed().as_secs_f32()
+    );
     let program = program!(&display, 140 => {
         vertex: "
         #version 140
         in vec2 position;
+        in vec2 texcoord;
 
-        out vec3 vColor;
+        out vec4 vColor;
+        out vec2 vTexCoord;
 
         void main() {
             gl_Position = vec4(position, 0.0, 1.0);
-            vColor = vec3(1.0, 1.0, 1.0);
+            vColor = vec4(1.0, 1.0, 1.0, 1.0);
+            vTexCoord = texcoord;
         }
         ",
         fragment: "
         #version 140
-        in vec3 vColor;
+        in vec4 vColor;
+        in vec2 vTexCoord;
+
         out vec4 f_color;
 
+        uniform sampler2D image;
+
         void main() {
-            f_color = vec4(vColor, 1.0);
+            f_color = texture(image, vTexCoord) * vColor;
         }
         ",
     })
     .unwrap();
 
-    let uniforms = uniform! {};
+    let tex = construct_texture_from_imagefile(&display, &file_path).unwrap();
+    log_verbose!(
+        "Total time since app start: {}ms",
+        start_instant.elapsed().as_millis()
+    );
 
     el.run(move |event, _target, control| {
         use glium::Surface;
         use glutin::event::{Event, WindowEvent};
         use glutin::event_loop::ControlFlow;
 
-        let mut frame = display.draw();
-        frame.clear(None, None, false, None, None);
-        frame
-            .draw(&vbo, &ibo, &program, &uniforms, &Default::default())
-            .unwrap();
-        frame.finish().unwrap();
+        let draw = || {
+            // build uniforms
+            let sampler = tex.sampled();
+            let uniforms = uniform! { texture: sampler };
+
+            // draw a frame
+            let mut frame = display.draw();
+            frame.clear(None, None, false, None, None);
+            frame
+                .draw(&vbo, &ibo, &program, &uniforms, &Default::default())
+                .unwrap();
+            frame.finish().unwrap();
+        };
+
+        draw();
 
         match event {
+            // TODO: Window resizing
             Event::WindowEvent { event: we, .. } => {
                 if let WindowEvent::CloseRequested = we {
                     *control = ControlFlow::Exit;
